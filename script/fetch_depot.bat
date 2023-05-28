132c27a852a488aff9aed8bdbc87f73bc5e759cf
iapoo <xiongbo.yang@roadefend.com>
2023-05-28 20:52:58 +0800

    init

---
+++ script/fetch_depot.bat
@@ -0,0 +1,3 @@
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+set DEPOT_TOOLS_DIR=./depot_tools
+set PATH=%DEPOT_TOOLS_DIR%;%PATH%
---
+++ script/fetch_skia.bat
@@ -0,0 +1,4 @@
+git clone https://skia.googlesource.com/skia.git
+cd skia
+python3 tools/git-sync-deps
+bin/fetch-ninja
